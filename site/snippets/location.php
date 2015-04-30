1aaba1de469e110b2c05238e5c6fd8351ccc3c82
mayawagon <mayawagon@gmail.com>
2015-04-30 16:53:51 -0400

    added border around address and link to google map

--- site/snippets/location.php
+++ site/snippets/location.php
@@ -1,13 +1,15 @@
 <section class="location">
 	<h2>Location</h2>
-	<div class="vcard"> 
-	   <div class="adr"> 
-	   		<div class="room-name">Horace Mann Room</div>
-	   		<div class="room-name">Microsoft NERD</div>
-	      	<div class="street-address">1 Memorial Drive #1</div> 
-	      	<div class="locality">Cambridge, MA</div>
-	      	<div class="postal-code">02142</div>  
-	   </div> 
-	</div>
+	<a href="https://www.google.com/maps/place/Microsoft+New+England+Research+and+Development+Center/@42.361369,-71.081355,17z/data=!3m1!4b1!4m2!3m1!1s0x89e370a423d61825:0x58516248462c99eb">
+		<div class="vcard"> 
+	   		<div class="adr"> 
+	   			<div class="room-name">Horace Mann Room</div>
+	   			<div class="room-name">Microsoft NERD</div>
+	      		<div class="street-address">1 Memorial Drive #1</div> 
+	      		<div class="locality">Cambridge, MA</div>
+	      		<div class="postal-code">02142</div>  
+	   		</div> 
+		</div>
+	</a>
 	<iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/mayawagon.m2bo32ji/attribution,zoompan,geocoder,share.html?access_token=pk.eyJ1IjoibWF5YXdhZ29uIiwiYSI6Im81VmVMRE0ifQ.AQStgQMPXycvI8M3l0_ZGA'></iframe>
 </section>
