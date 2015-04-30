296459ab3e7c6ea0596def60b751f0779d1ad937
mayawagon <mayawagon@gmail.com>
2015-04-30 16:46:02 -0400

    added a location section with map and address

---
+++ site/snippets/location.php
@@ -0,0 +1,13 @@
+<section class="location">
+	<h2>Location</h2>
+	<div class="vcard"> 
+	   <div class="adr"> 
+	   		<div class="room-name">Horace Mann Room</div>
+	   		<div class="room-name">Microsoft NERD</div>
+	      	<div class="street-address">1 Memorial Drive #1</div> 
+	      	<div class="locality">Cambridge, MA</div>
+	      	<div class="postal-code">02142</div>  
+	   </div> 
+	</div>
+	<iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/mayawagon.m2bo32ji/attribution,zoompan,geocoder,share.html?access_token=pk.eyJ1IjoibWF5YXdhZ29uIiwiYSI6Im81VmVMRE0ifQ.AQStgQMPXycvI8M3l0_ZGA'></iframe>
+</section>
--- site/templates/conference-onepager.php
+++ site/templates/conference-onepager.php
@@ -8,7 +8,7 @@
 				<p><?php echo $page->text() ?></p>
 			</section>
 			<?php snippet('schedule') ?>
-			<iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/mayawagon.m2bo32ji/attribution,zoompan,zoomwheel,geocoder,share.html?access_token=pk.eyJ1IjoibWF5YXdhZ29uIiwiYSI6Im81VmVMRE0ifQ.AQStgQMPXycvI8M3l0_ZGA'></iframe>
+			<?php snippet('location') ?>
 		</div>
 	</div>
 </body>
