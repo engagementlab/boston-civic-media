4090444d6bd5e05c09bf47b31c7fae52078aadf7
Maya Wagoner <mayawagon@gmail.com>
2015-05-06 16:26:43 -0400

    made schedule pretty neat

--- site/snippets/schedule.php
+++ site/snippets/schedule.php
@@ -4,15 +4,20 @@
 		<?php foreach($page->children()->find('schedule')->children() as $session): ?>
 		
 		<li class="session">
-			<time><?php echo $session->time()?></time>
+			<div class="time"><time><?php echo $session->time()?></time></div>
 			<div class="event-info">
 				<h3 class="session-title"><?php echo $session->title() ?></h3>
-				<p class="byline"><?php echo $session->speaker() ?></p>
-				<p><a class="speaker-link" href="<?php echo $session->speakerlink() ?>"><?php echo $session->linktext() ?></a></p>
-				<div>
-					<p class="event-description"><?php echo $session->description() ?><p>
-				</div>
-			</div>
+				<?php if(!$session->speaker()->empty()): ?>
+					<p class="byline">
+						<?php echo $session->speaker() ?>, <a class="speaker-link" href="<?php echo $session->speakerlink() ?>"><?php echo $session->linktext() ?></a>
+						</p>
+				<?php endif ?>
+				<?php if(!$session->description()->empty()): ?>
+					<div>
+						<p class="event-description"><?php echo $session->description() ?><p>
+					</div>
+				<?php endif ?>
+			<div class="clear" />
 		</li>
 
 		<?php endforeach ?>
