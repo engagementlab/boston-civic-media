e14b833d2203fa559985706980fda6a0aad7f0ab
Erica Marchman Salling <esalling23@gmail.com>
2016-05-16 11:00:29 -0400

    up

--- app.js
+++ app.js
@@ -11,10 +11,16 @@ appStart = function(app) {
 
 };
 
-module.exports = function() {
+module.exports = function(frameworkDir) {
 
+	// Add main dependencies and EL web framework dependencies
+	require('app-module-path').addPath(__dirname + '/node_modules'); 
+	require('app-module-path').addPath(frameworkDir + '/node_modules'); 
+	
+	// Obtain app root path and set as keystone's module root
+	var appRootPath = require('app-root-path').path;
 	var keystoneInst = require('keystone');	
-	keystoneInst.set('module root', __dirname);
+	keystoneInst.set('module root', appRootPath);
 
 	return { 
 
