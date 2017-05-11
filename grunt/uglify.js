878e67526ac1ff0dbab365abc6b5a076e24063bf
Johnny Richardson <johnny.c.rich@gmail.com>
2017-05-11 14:37:56 -0400

    Uglify and cdnAsset fix.

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,8 +1,25 @@
 // Site-wide JS
 module.exports = function() {
+  // Obtain env to generate filename
+  var env = grunt.option('env');
+
+  if(env === undefined) {
+    
+    grunt.log.writeln('No env provided, checking NODE_ENV');
+    
+    if(process.env.NODE_ENV !== undefined)
+      env = process.env.NODE_ENV;
+    else {
+      grunt.log.subhead('No env provided, defaulting to production!');
+      env = 'production'
+    }
+
+  }
+
+  grunt.log.writeln('Compiling ' + env + '.js');
 
   // Output file is relative to this site
-  var fileOut = __dirname + '/../public/release/production.js';
+  var fileOut = __dirname + '/../public/release/' + env + '.js';
   var config = { uglify: { files: {} } };
 
   // Files to uglify
@@ -13,6 +30,7 @@ module.exports = function() {
     __dirname + '/../public/plugins/**/*.js',
     '!' + __dirname + '/../public/plugins/isotope/*.js',  // Ignore isotope (this is a seperate prod include)
     __dirname + '/../public/plugins/bxslider/jquery.bxslider.js',
+    __dirname + '/../public/bower_components/cloudinary-core/cloudinary-core-shrinkwrap.js'
   
   ];
 
