8da4a81921801db9c6557d00d6f263a1f6ffe545
Johnny Richardson <johnny.c.rich@gmail.com>
2017-05-22 16:19:54 -0400

    Revamped getting started page.

--- models/ListservGuidelines.js
+++ models/ListservGuidelines.js
@@ -32,7 +32,6 @@ var ListservGuidelines = new keystone.List('ListservGuidelines',
  * @main Listserv
  */
 ListservGuidelines.add({
-	name: { type: String, hidden: true, required: true, initial: true },
 	question: { type: String, label: "Question",  initial: true, required: true },
 	answer: { type: Types.Textarea, label: "Answer",  initial: true, required: true },
 	createdAt: { type: Date, default: Date.now, noedit: true, hidden: true }
@@ -42,5 +41,5 @@ ListservGuidelines.add({
  * Model Registration
  */
 ListservGuidelines.defaultSort = '-createdAt';
-ListservGuidelines.defaultColumns = 'name, question, answer';
+ListservGuidelines.defaultColumns = 'question';
 ListservGuidelines.register();
